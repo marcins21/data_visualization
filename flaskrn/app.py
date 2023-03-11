8d09bdb2cf77770312cd9c0e7b496523be726dd6
idontfeelsogoood <msitko21@interia.pl>
2023-03-11 17:07:59 +0100

    refactor Structure

--- flaskrn/app.py
+++ flaskrn/app.py
@@ -1,14 +1,12 @@
 from flask import Flask,render_template
 
-
-
 app = Flask(__name__)
    
 
 @app.route("/")
 def home():
     d = {"marcin":15,"tomek":20,"artur":50}
-    return render_template("landing_page.html",my_dict = d)
+    return render_template("landing_page.html",my_dict=d)
 
 if __name__ == "__main__":
    
