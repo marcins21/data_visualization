b84e030877793c795c5842b6b5230a6e6a103fdf
idontfeelsogoood <msitko21@interia.pl>
2023-03-11 17:22:57 +0100

    info func

--- flaskrn/app.py
+++ flaskrn/app.py
@@ -6,7 +6,15 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     d = {"marcin":15,"tomek":20,"artur":50}
+    info_func(kwargs=d)
+
     return render_template("landing_page.html",my_dict=d)
+
+def info_func(*args,**kwargs):
+    new_list = {**kwargs}
+    print(f"\n\nDictionary Given to Landing_page.html \n{new_list}\n")
+
+
 
 if __name__ == "__main__":
    
